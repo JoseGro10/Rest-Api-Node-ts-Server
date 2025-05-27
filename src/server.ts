c2f0bded9774a6ede9c13e81f3c7fa21a160e205
JoseGro10 <edgardo30g@gmail.com>
2025-05-27 09:05:14 -0600

    Diferentes metodos HTTP y probando con Postman

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,20 @@ server.get('/', (req, rs) => {
     rs.send('¡Hola, mundo! Esta es una API REST con Express y TypeScript.') 
 })
 
+server.post('/', (req, res) => {
+    res.json("Desde POST: ¡Hola, mundo! Esta es una API REST con Express y TypeScript.")
+})
+
+server.put('/', (req, res) => {
+    res.json("Desde PUT: ¡Hola, mundo! Esta es una API REST con Express y TypeScript.")
+})
+
+server.patch('/', (req, res) => {
+    res.json("Desde PATCH: ¡Hola, mundo! Esta es una API REST con Express y TypeScript.")
+})
+
+server.delete('/', (req, res) => {
+    res.json("Desde DELETE: ¡Hola, mundo! Esta es una API REST con Express y TypeScript.")
+})
+
 export default server
