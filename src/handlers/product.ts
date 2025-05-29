cddfd81da0d298409589c2c06219d210ea60a80d
JoseGro10 <edgardo30g@gmail.com>
2025-05-29 11:22:27 -0600

    Moviendo la validacion hacia el router

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -4,14 +4,6 @@ import Product from '../models/Product.model'
 
 export const createProduct = async (req: Request, res : Response) => {
 
-    //Validacion
-    await check('name', 'El nombre es obligatorio').notEmpty().run(req)
-    await check('price')
-        .isNumeric().withMessage('Valor no valido')
-        .notEmpty().withMessage('El precio no puede ir vacio')
-        .custom( value => value > 0 ).withMessage('El precio no puede ser menor o igual a 0')
-        .run(req)
-
     let errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createProduct } from './handlers/product'
 
 const router = Router()
@@ -8,7 +9,16 @@ router.get('/', (req, rs) => {
     rs.send('¡Hola, mundo! Esta es una API REST con Express y TypeScript.') 
 })
 
-router.post('/', createProduct)
+router.post('/', 
+    //Validacion
+    body('name', 'El nombre es obligatorio').notEmpty(),
+    body('price')
+        .isNumeric().withMessage('Valor no valido')
+        .notEmpty().withMessage('El precio no puede ir vacio')
+        .custom( value => value > 0 ).withMessage('El precio no puede ser menor o igual a 0'),
+    
+    createProduct
+)
 
 router.put('/', (req, res) => {
     res.json("Desde PUT: ¡Hola, mundo! Esta es una API REST con Express y TypeScript.")
